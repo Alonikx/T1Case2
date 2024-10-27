79f2146a160d29645ee136e25d5eea21149ce09a
Legeonery <117186553+Legeonery@users.noreply.github.com>
2024-10-27 10:34:54 +0400

    #front

--- datasets/main.js
+++ datasets/main.js
@@ -10,13 +10,19 @@ function addRow() {
     const headerCells = document.getElementById('header-row').children;
     for (let i = 1; i < headerCells.length; i++) {
         const cell = document.createElement('td');
-        cell.innerHTML = '<input type="text" placeholder="Введите данные">';
+        cell.innerHTML = '<input type="text" value="-" form="form" placeholder="Введите данные">';
         newRow.appendChild(cell);
     }
 
     tbody.appendChild(newRow);
 }
 
+window.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && (event.key === 'к'||event.key === 'r')) {
+        event.preventDefault();
+        addRow();
+    }
+});
 function deleteRow(btn) {
     const row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
@@ -25,20 +31,32 @@ function deleteRow(btn) {
 function addColumn() {
     const headerRow = document.getElementById('header-row');
     const newHeaderCell = document.createElement('th');
-    newHeaderCell.innerHTML = `<button style="background:none;border:none" onclick="deleteColumn(this)"><img src="./close.svg" width="16px" heigth="16px"/></button>`;
+    const headerCells = document.getElementById('header-row').children;
+    var int=0;
+    for (let i = 1; i < headerCells.length; i++) {
+        int++;
+    }
+    newHeaderCell.innerHTML = `<button style="background:none;border:none" onclick="deleteColumn(this)"><img src="./close.svg" width="16px" heigth="16px"/></button><input type="text" class="${int}" placeholder="Поиск имен.." form="false">`;
     headerRow.appendChild(newHeaderCell);
     const input = document.createElement('input');
     input.type = 'text';
+    input.className = `input`;
     input.value= "Новый столбец";
     newHeaderCell.appendChild(input);
 
     const rows = document.getElementById('tbody').children;
     for (let i = 0; i < rows.length; i++) {
         const newCell = document.createElement('td');
-        newCell.innerHTML = '<input type="text" placeholder="Введите данные">';
+        newCell.innerHTML = '<input type="text" form="form" value="-" placeholder="Введите данные">';
         rows[i].appendChild(newCell);
     }
 }
+window.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && (event.key === 'c'||event.key === 'с')) {
+        event.preventDefault();
+        addColumn();
+    }
+});
 
 function deleteColumn(btn) {
     const headerRow = document.getElementById('header-row');
